05e9da706573fb02fb8e2bba62928bcace80b24c
addisonkline <77369109+addisonkline@users.noreply.github.com>
2025-12-16 15:57:09 -0500

    basic notebook work

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pitcher-rating!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/pitcher_rating/main.py
@@ -0,0 +1,9 @@
+import argparse
+
+
+def main() -> None:
+    print("Hello, world!")
+
+
+if __name__ == "__main__":
+    main()
